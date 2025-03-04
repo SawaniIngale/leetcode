6ccc33efc8623aaab3ec532b42e3fc169d57785a
SawaniIngale <143910844+SawaniIngale@users.noreply.github.com>
2025-03-04 14:43:16 -0800

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.62%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #brute force that can handle edge cases : Time complexity O(n^2) as we are traversing through all elements in O(n) and then taking the min for which we have to scan the whole array in O(n)
+        # count = 0
+        # while any(nums):
+        #     min_val = min([num for num in nums if num > 0])
+        #     nums = [num - min_val if num > 0 else 0 for num in nums]
+        #     count += 1
 
-        count = 0
-        while any(nums):
-            min_val = min([num for num in nums if num > 0])
-            nums = [num - min_val if num > 0 else 0 for num in nums]
-            count += 1
+        # return count
 
-        return count
+        # Optimized approach using sets: Sets will give us unique elements and if we minus the 0's then we have unique numbers whose length will the number of operations needed to make all elements to 0 : Time complexity o(n)
+        # set(nums): O(n) to create a set,  -{0} O(1) to remove zeros, len(set) O(1)
+
+        return len(set(nums) - {0})
+
         
