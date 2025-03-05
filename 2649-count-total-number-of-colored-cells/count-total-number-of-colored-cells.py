0f1aa056905c862b7cd9e8d3cc3fa294c485965f
SawaniIngale <143910844+SawaniIngale@users.noreply.github.com>
2025-03-04 18:29:02 -0800

    Time: 1 ms (30.77%) | Memory: 12.4 MB (69.23%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def coloredCells(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result = n**2 + (n-1)**2
+
+        return result
+        
