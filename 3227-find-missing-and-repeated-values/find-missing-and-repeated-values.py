99b306164da9c5ff04105310a93ce1236e381496
SawaniIngale <143910844+SawaniIngale@users.noreply.github.com>
2025-03-05 18:10:53 -0800

    Time: 15 ms (42.98%) | Memory: 13 MB (14.33%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -6,12 +6,14 @@ class Solution(object):
         """
         n = len(grid)
         count = {i:0 for i in range(1, n*n + 1)}
+       # n = 2 , range = (1,5)
+       # count = {1:0, 2:0, 3:0, 4:0}
 
-        for row in grid:
-            for num in row:
-                count[num] += 1
+        for row in grid: # for each row
+            for num in row: # for every num in that particular row
+                count[num] += 1 
 
-        duplicate, missing = -1, -1
+        duplicate, missing = -1, -1 #Not setting to 0 to not mislead
         for num in count:
             if count[num] == 2:
                 duplicate = num
