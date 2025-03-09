83899e8ced0c2e4df5367f2590c5948ff19fe056
SawaniIngale <143910844+SawaniIngale@users.noreply.github.com>
2025-03-08 23:36:18 -0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (45.43%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -4,8 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        word = s.split()
-        return len(word[-1])
+        # word = s.split()
+        # return len(word[-1])
 
-        
+        #-------- to do inplace iterating from end
+
+        i = len(s)-1
+
+        while i >= 0 and s[i] == " ":
+            i -= 1
+        length = 0
+        while i >= 0 and s[i]!= " ":
+            length += 1
+            i -= 1
+
+        return length
+
+
+
         
