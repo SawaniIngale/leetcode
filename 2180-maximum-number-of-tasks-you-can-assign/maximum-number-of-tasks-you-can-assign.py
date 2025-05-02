9f5dd8649da241cf03d7e0e21ec0b2bff22a26dc
SawaniIngale <143910844+SawaniIngale@users.noreply.github.com>
2025-05-01 17:00:43 -0700

    Time: 3384 ms (70.00%) | Memory: 18.6 MB (80.00%) - LeetSync

---
+++ 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def maxTaskAssign(self, tasks, workers, pills, strength):
+        """
+        :type tasks: List[int]
+        :type workers: List[int]
+        :type pills: int
+        :type strength: int
+        :rtype: int
+        """
+        n, m = len(tasks), len(workers)
+        tasks.sort()
+        workers.sort()
+
+        def check(mid):
+            p = pills
+            # Ordered set of workers
+            ws = SortedList(workers[m - mid :])
+            # Enumerate each task from largest to smallest
+            for i in range(mid - 1, -1, -1):
+                # If the largest element in the ordered set is greater than or equal to tasks[i]
+                if ws[-1] >= tasks[i]:
+                    ws.pop()
+                else:
+                    if p == 0:
+                        return False
+                    rep = ws.bisect_left(tasks[i] - strength)
+                    if rep == len(ws):
+                        return False
+                    p -= 1
+                    ws.pop(rep)
+            return True
+
+        left, right, ans = 1, min(m, n), 0
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
+
+        
+        
